d56fa41fbb7859922a6db6530f3979b75084adaa
marcelo-rebello1982 <mp.rebello.martins@gmail.com>
2021-08-25 18:28:53 -0300

    chore: alterado style.css

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,36 @@
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+import '../styles/room.scss'
+
+
 export function Room() {
+
+    
     return (
-        <h1>Sala</h1>
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="LetMeask" />
+                    <div>codigo</div>
+                </div>
+            </header>
+
+            <main className="content">
+                <div className="room-title">
+                    <h1>Sala React</h1>
+                    <span>4 perguntas</span>
+                </div>
+                <form>
+                    
+                    <textarea
+                     placeholder="O Que você quer perguntar ?"
+                />
+                <div className="form-footer">
+                        <span>Para enviar sua pergunta, <button>faça seu login</button>.</span>
+                            <Button type ="submit">Enviar Perguta</Button>
+                            </div>
+                </form>
+            </main>
+        </div>
     );
 }
