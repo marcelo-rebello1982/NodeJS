06615b667f2914c267a8256330994e6c56092ffb
marcelo-rebello1982 <mp.rebello.martins@gmail.com>
2021-08-25 14:16:05 -0300

    fix: ajustes do componente

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,6 @@ import '../styles/button.scss';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-
 export function Button(props: ButtonProps) {
     return (
         <button className="button" {...props} />
