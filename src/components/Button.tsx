ae5de20689848355c410f2474cbbe5e79e12cba8
marcelo-rebello1982 <mp.rebello.martins@gmail.com>
2021-08-25 14:19:04 -0300

    fix: ajustes no componente

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 import { ButtonHTMLAttributes } from 'react'
 import '../styles/button.scss';
 
+// types do typeScript
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export function Button(props: ButtonProps) {
